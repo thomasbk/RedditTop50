e0036fe5d668b5129150aa3cf89e43500b2028be
Thomas Baltodano <thomasbk@Thomas-Baltodanos-MacBook-Pro.local>
2017-07-14 13:40:36 -0600

    Initial Commit

---
+++ RedditTop50/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedditTop50
+//
+//  Created by Thomas Baltodano on 7/14/17.
+//  Copyright © 2017 Thomas Baltodano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
