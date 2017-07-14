4db11583859085814ce5336b0e13d966afe8c3c7
Thomas Baltodano <thomasbk@Thomas-Baltodanos-MacBook-Pro.local>
2017-07-14 14:36:22 -0600

    Created base tableview

--- RedditTop50/ViewController.swift
+++ RedditTop50/ViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView!
+    let cellIdentifier = "redditCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,48 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let numberOfRows = 5
+        return numberOfRows
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath as IndexPath) as! RedditTableViewCell
+        
+        
+        cell.titleLabel.text = "This is the title"
+        cell.dateLabel.text = "10 hours ago by Albert"
+        cell.commentsLabel.text = "44 comments"
+        
+        //let url = URL(string: "https://httpbin.org/image/png")!
+        //cell.myImageView.image
+        
+        
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    // MARK: -
+    // MARK: Table View Delegate Methods
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
 
 
---
+++ RedditTop50/Views/RedditTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  RedditTableViewCell.swift
+//  RedditTop50
+//
+//  Created by Thomas Baltodano on 7/14/17.
+//  Copyright © 2017 Thomas Baltodano. All rights reserved.
+//
+
+import UIKit
+
+class RedditTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var commentsLabel: UILabel!
+    @IBOutlet var myImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
